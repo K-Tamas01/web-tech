9da3bb1f4e4ef39dac022eff62a1265ca48e1bef
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-06 21:11:27 +0100

    add other routings

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -7,7 +7,7 @@ const auth = require('../controller/Auth/Auth.controller');
 export default async(fastify: FastifyInstance):Promise<void> =>{
   fastify.post('/login',{handler: login.loginCtrl});
   fastify.post('/sign-up',{handler: signup.signupnCtrl})
-  fastify.put('/update-data',{preHandler: auth, handler: login.updateAcc})
-  fastify.delete('/delete-account',{preHandler: auth, handler: login.deleteAcc})
-  fastify.post('/options',{preHandler: auth, handler: login.getAccData})
+  fastify.put('/update-data',{preHandler: auth.authenticationwithjwttoken, handler: login.updateAcc})
+  fastify.delete('/delete-account',{preHandler: auth.authenticationwithjwttoken, handler: login.deleteAcc})
+  fastify.post('/options',{preHandler: auth.authenticationwithjwttoken, handler: login.getAccData})
   }
