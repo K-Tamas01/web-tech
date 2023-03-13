8bef791dca9b0c72fe6a856a0ae078be60bc741a
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-13 16:25:27 +0100

    Update user.context.jsx

--- frontend/src/Context/user.context.jsx
+++ frontend/src/Context/user.context.jsx
@@ -1,9 +1,9 @@
 import { createContext, useState } from "react";
 
-export const UserContext = createContext(null);
+export const UserContext = createContext(undefined);
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({name: null, email: null, id: null})
+    const [user, setUser] = useState({name: undefined, email: undefined, id: undefined})
 
     return <UserContext.Provider value={{ user, setUser }}>{ children }</UserContext.Provider>;
 }
