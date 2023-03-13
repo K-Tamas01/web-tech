2cb5415922bb937ae4b8037837fcec18b1569571
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-13 16:25:03 +0100

    Create Options.routes.jsx

---
+++ frontend/src/routes/Options/Options.routes.jsx
@@ -0,0 +1,11 @@
+import OptionsPage from '../../Components/Options/options.components';
+
+const Options = () =>{
+    return(
+        <div>
+            <OptionsPage />
+        </div>
+    )
+}
+
+export default Options;
