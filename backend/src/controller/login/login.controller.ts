1ec627b1203825fdb0b9ea7906ca8e51f459f5be
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-20 18:29:53 +0100

    Update login.controller.ts

--- backend/src/controller/login/login.controller.ts
+++ backend/src/controller/login/login.controller.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { IBodyLogin, IbodyLoginString, IjwttokenHeader } from '../../interfaces/request.interfaces';
+import { IBodyLogin, IbodyLoginString } from '../../interfaces/request.interfaces';
 
 const login = require('../../model/user.scehma');
 const jwt = require('jsonwebtoken');
@@ -11,8 +11,8 @@ const loginCtrl = async(req: FastifyRequest<{Body: IBodyLogin}>, rep: FastifyRep
 
     const result = await login.findOne({email: email});
 
-    if(!result) return rep.code(404).send({msg: 'Nincs ilyen Email cím!'});
-    if(result.password !== md5(password)) return rep.code(400).send({msg: 'Hibás jelszó'});
+    if(!result) return rep.code(404).send('Nincs ilyen Email cím!');
+    if(result.password !== md5(password)) return rep.code(400).send('Hibás jelszó');
 
     const payload = {
       Email: result.email,
@@ -26,7 +26,15 @@ const loginCtrl = async(req: FastifyRequest<{Body: IBodyLogin}>, rep: FastifyRep
       {expiresIn: 60 * 60 * 1 } //1 óra
     )
 
-    rep.code(200).header('set-cookie', token).send({ID: result._id, Username: result.Uname, Email: result.email});
+    rep.setCookie('user-access', token,{
+      path:'http://localhost:5000/',
+      sameSite: 'strict',
+      maxAge: 3600, 
+      httpOnly: true,
+      secure: false,
+    })
+
+    rep.code(200).header('Access-Control-Expose-Headers', 'set-cookie').send({ID: result._id, Username: result.Uname, Email: result.email});
 };
 
 const deleteAcc = async (req: FastifyRequest<{Body: IbodyLoginString}>, rep: FastifyReply) => {
@@ -35,7 +43,7 @@ const deleteAcc = async (req: FastifyRequest<{Body: IbodyLoginString}>, rep: Fas
 
   if(result.deletedCount === 0) rep.code(400).send({msg: 'Hiba történt...'});
 
-  rep.code(200).send(result);
+  rep.code(200).send({msg: 'Sikeres'});
   
 };
 
@@ -44,25 +52,48 @@ const updateAcc = async (req: FastifyRequest<{Body: IbodyLoginString}>, rep: Fas
 
   const result = await login.findOne({email: oldEmail});
 
-  if(!result) rep.code(400).send({msg: 'Hiba történt...'});
+  if(!result) return rep.code(400).send({msg: 'Hiba történt...'});
 
   if(newEmail !== '') {
     const update = await login.updateOne({email: oldEmail}, {email: newEmail}, {upsert: false})
-    if(!update) rep.code(400).send({msg: 'Hiba történt...'})
+    if(!update) return rep.code(400).send({msg: 'Hiba történt...'})
   }
 
-  if(md5(oldPassword) == result.password) rep.code(400).send({msg: 'Hibás jelszó!'})
+  if(oldPassword !== '') {
+    if(md5(oldPassword) == result.password) return rep.code(400).send({msg: 'Hibás jelszó!'})
 
-  if(newPassword !== '') {
-    const update = await login.updateOne({email: oldEmail}, {password: md5(newPassword)}, {upsert: false})
-    if(!update) rep.code(400).send({msg: 'Hiba történt...'})
+    if(newPassword !== '') {
+      const update = await login.updateOne({email: oldEmail}, {password: md5(newPassword)}, {upsert: false})
+      if(!update) return rep.code(400).send({msg: 'Hiba történt...'})
+    }
   }
 
-  newEmail ? rep.code(200).send({msg: 'Sikeres változtatás', email: newEmail}) : rep.code(200).send({msg: 'Sikeres változtatás'});
+  const payload = {
+    Email: newEmail !== '' ? newEmail : result.email,
+    Password: newPassword !== '' ? newPassword: result.password,
+    _id: result._id
+  };
+  
+  const token = jwt.sign(
+    payload,
+    process.env.MY_SECRECT_TOKEN,
+    {expiresIn: 60 * 60 * 1 } //1 óra
+  )
+
+  rep.setCookie('user-access', token,{
+    path:'http://localhost:5000/',
+    sameSite: 'strict',
+    maxAge: 3600, 
+    httpOnly: true,
+    secure: false,
+  })
+
+  newEmail ? rep.code(200).header('Access-Control-Expose-Headers', 'set-cookie').send({msg: 'Sikeres változtatás', email: newEmail}) : rep.code(200).header('Access-Control-Expose-Headers', 'set-cookie').send({msg: 'Sikeres változtatás'});
 };
 
-const getAccData = async (req: FastifyRequest<{Headers: IjwttokenHeader}>, rep: FastifyReply) => {
-  const {token} = req.headers;
+const getAccData = async (req: FastifyRequest, rep: FastifyReply) => {
+
+  const token = req.headers['cookie'];
 
   const decoded = jwt.verify(token, process.env.MY_SECRET_KEY);
 
