809dea288ff6e42294d9a450e5105613fab34268
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-23 18:03:42 +0100

    Update users.ts

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -5,7 +5,7 @@ const signup = require('../controller/sign-up/sign-up.controller');
 const auth = require('../controller/Auth/Auth.controller');
 
 export default async(fastify: FastifyInstance):Promise<void> =>{
-  fastify.post('/login',{handler: login.loginCtrl});
+  fastify.post('/login',{handler: login.loginCtrl})
   fastify.post('/sign-up',{handler: signup.signupnCtrl})
   fastify.put('/update-data',{preHandler: auth.authenticationwithjwttoken, handler: login.updateAcc})
   fastify.delete('/delete-account',{preHandler: auth.authenticationwithjwttoken, handler: login.deleteAcc})
