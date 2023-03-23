f28254e0d7ec4876495e2c46d8186d5b46371deb
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-23 18:04:12 +0100

    Update cinema.controller.ts

--- backend/src/controller/cinema/cinema.controller.ts
+++ backend/src/controller/cinema/cinema.controller.ts
@@ -5,6 +5,8 @@ const Cinema = require('../../model/cinema.schema');
 const getCinemaCtrl = async (req: FastifyRequest,rep:FastifyReply) =>{
 
     const result = await Cinema.find();
+
+    if(!result) return rep.code(400).send({msg: 'Hiba történt a lekérdezéskor!'});
   
     rep.code(200).send(result);
 }
