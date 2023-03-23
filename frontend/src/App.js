717b496036d2454c3edc6b26431ba892ad9353a8
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-03-23 18:03:27 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Films from './routes/Films/Film.route';
 import Cinemas from './routes/Cinemas/Cinema.route';
 import Auth from './routes/Auth/Auth.routes';
 import Options from './routes/Options/Options.routes';
+import FilmUplaod from "./routes/Film-upload/FilmUpload.routes";
 
 import AlertBox from './Components/Alert/alert.components';
 
@@ -31,6 +32,7 @@ const App = () =>{
         <Route path="Vetítés" element={<Films />} />
         <Route path="Mozi" element={<Cinemas />} />
         <Route path="Auth" element={<Auth messageText={getMessage} messageType={getMessageType} />} />
+        <Route path="Films-form" element={<FilmUplaod messageText={getMessage} messageType={getMessageType} />} />
         <Route path="Options" element={<Options messageText={getMessage} messageType={getMessageType} />} />
       </Route>
     </Routes>
